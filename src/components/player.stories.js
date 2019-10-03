d06926982aea5a5004e9aaddb724764383bbb96e
Grant Ammons <gammons@gmail.com>
2019-10-03 06:21:14 -0400

    let's actually be responsible

--- src/components/player.stories.js
+++ src/components/player.stories.js
@@ -7,11 +7,9 @@ import SpotifyLogin from "./spotifyLogin"
 
 storiesOf("Player", module)
   .add("simple", () => {
-    const token =
-      "BQBZuloBmn5YHbmJ3pfXAOh_gDROO95aM08CNkthIcGUbc31Bg-Dgk1XS65OOlqexVlV99YbEmCND_GyIQNq2Mwgjnkhdq9Byb_aUgHHBtymtN0qZ22zMM3MzvrJzdIf1VgrOenfl7ryw7S_S-vsYpINg1q0VMJFmFE9Wfg"
     return (
       <Player
-        accessToken={token}
+        accessToken={process.env.SPOTIFY_TOKEN}
         uri={"spotify:album:3DrgM5X3yX1JP1liNLAOHI"}
       />
     )
