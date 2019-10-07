56e3a00e7bdfe215fa99758dd9ab7f761905393e
Grant Ammons <gammons@gmail.com>
2019-10-07 15:21:16 -0400

    show something

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from "react"
-import { getAlbums } from "./backend"
+
 import Album from "./components/album"
+import AlbumModel from "./models/album"
 
 import Header from "./components/header"
 import SearchBar from "./components/searchBar"
 import Footer from "./components/footer"
 
 const App = () => {
+  const [albums, setAlbums] = useState([])
+
+  useEffect(() => {
+    fetch("first-albums.json")
+      .then(data => data.json())
+      .then(data => {
+        setAlbums(data.albums.map(a => new AlbumModel(a)))
+      })
+  }, [])
+
   return (
     <div className="flex flex-col items-stretch min-h-screen">
       <Header />
       <SearchBar />
-      <div className="flex-grow">
-        <p>Albums</p>
+      <div className="flex-grow flex flex-row flex-wrap bg-gray-100 p-8">
+        {albums.map((album, count) => (
+          <Album key={count} album={album} />
+        ))}
       </div>
       <Footer />
     </div>
