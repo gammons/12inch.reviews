cda4c5923adb1093140843dc4d8dbbc574b71064
Grant Ammons <gammons@gmail.com>
2019-10-09 07:14:59 -0400

    make search bar look somewhat better

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -7,7 +7,7 @@ const SearchBar = props => {
   const labelVars = "text-gray-500 block text-xs uppercase font-bold"
   return (
     <div className="w-full bg-gray-200 p-4 px-8 flex flex-row flex-wrap">
-      <div className="w-full py-1">
+      <div className="w-full md:w-4/12 py-1">
         <label className={labelVars}>Search:</label>
         <input
           className="border-2 border-gray-300 bg-white h-12 px-5 text-gray-700 rounded-lg focus:outline-none w-full"
@@ -16,33 +16,35 @@ const SearchBar = props => {
         />
       </div>
 
-      <div className="w-full md:w-48 py-1 md:mr-4">
-        <label className={labelVars}>Genre:</label>
-        <select className={cssVars}>
-          <option>All genres</option>
-          <option>Rock</option>
-          <option>Electronic</option>
-        </select>
-      </div>
+      <div className="w-full md:w-6/12 flex flex-row">
+        <div className="w-full md:w-48 py-1 md:mr-4">
+          <label className={labelVars}>Genre:</label>
+          <select className={cssVars}>
+            <option>All genres</option>
+            <option>Rock</option>
+            <option>Electronic</option>
+          </select>
+        </div>
 
-      <div className="w-full md:w-48 py-1 md:mr-4">
-        <label className={labelVars}>Rating above:</label>
-        <input
-          className={cssVars}
-          type="number"
-          defaultValue="1.0"
-          step="0.5"
-          min="1"
-          max="10"
-        />
-      </div>
+        <div className="w-full md:w-48 py-1 md:mr-4">
+          <label className={labelVars}>Rating above:</label>
+          <input
+            className={cssVars}
+            type="number"
+            defaultValue="1.0"
+            step="0.5"
+            min="1"
+            max="10"
+          />
+        </div>
 
-      <div className="w-full md:w-64 py-1 md:mr-4">
-        <label className={labelVars}>Sort:</label>
-        <select className={cssVars}>
-          <option>Created</option>
-          <option>Rating</option>
-        </select>
+        <div className="w-full md:w-64 py-1 md:mr-4">
+          <label className={labelVars}>Sort:</label>
+          <select className={cssVars}>
+            <option>Created</option>
+            <option>Rating</option>
+          </select>
+        </div>
       </div>
     </div>
   )
