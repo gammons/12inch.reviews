2df5d31c02560429ce6fa0f33113fde96d0ce447
Grant Ammons <gammons@gmail.com>
2019-10-11 12:00:52 -0400

    make prod constant work

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,8 @@
 export const PROD_FUNCTIONS_URL = ""
 export const LOCAL_FUNCTIONS_URL = "http://localhost:9000"
 export const FUNCTIONS_URL =
-  process.env.NODE_ENV === "production"
-    ? PROD_FUNCTIONS_URL
-    : LOCAL_FUNCTIONS_URL
+  process.env.NODE_ENV === "development"
+    ? LOCAL_FUNCTIONS_URL
+    : PROD_FUNCTIONS_URL
 
 export const SPOTIFY_CLIENT_ID = "246fbd105e774530b341d50a7eedc0fc"
