144950c60ddaea3a4803650c802b343f91525a1f
Grant Ammons <gammons@gmail.com>
2019-10-13 11:27:21 -0400

    cleanup

--- .netlify/functions/spotifyLogin.js
+++ .netlify/functions/spotifyLogin.js
@@ -2,9 +2,6 @@ import fetch from "node-fetch"
 import formData from "form-data"
 
 exports.handler = async (event, context, callback) => {
-  console.log("NODE_ENV = ", process.env.NODE_ENV)
-  console.log("NODE_ENV = ", process.env.CONTEXT)
-
   const url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:9000"
