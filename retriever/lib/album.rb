c83c60c6965bcd0edc348960813224ef6ee02e9d
Grant Ammons <gammons@gmail.com>
2019-10-25 14:24:59 -0400

    remove unused function

--- retriever/lib/album.rb
+++ retriever/lib/album.rb
@@ -5,23 +5,4 @@ class Album < ActiveRecord::Base
   def timestamp=(t)
     self.created_at = Time.at(t / 1000)
   end
-
-  # def to_h
-  #   {
-  #     id: id,
-  #     timestamp: timestamp,
-  #     title: title,
-  #     artist: artist,
-  #     rating: rating,
-  #     bnm: bnm,
-  #     bnr: bnr,
-  #     label: label,
-  #     url: url,
-  #     description: description,
-  #     genre: genre,
-  #     spotify_album_id: spotify_album_id,
-  #     spotify_artist_id: spotify_artist_id,
-  #     image_url: image_url
-  #   }
-  # end
 end
