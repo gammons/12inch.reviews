09551045656ddee01afd388a23f7cb49bfe42347
Grant Ammons <gammons@gmail.com>
2019-11-01 15:20:36 -0400

    style up the volume knob

--- src/components/player/volumeSlider.js
+++ src/components/player/volumeSlider.js
@@ -1,6 +1,8 @@
 // @flow
 import React from "react"
 
+import "./range.css"
+
 type Props = {
   volume: number,
   onSetVolume: (val: number) => void
@@ -10,27 +12,14 @@ const VolumeSlider = (props: Props) => {
   return (
     <div>
       <input
+        className="range"
         type="range"
-        list="tickmarks"
         min={0}
         max={1}
         step={0.1}
         onChange={props.onSetVolume}
         value={props.volume}
       />
-      <datalist id="tickmarks">
-        <option value="0" label="0%" />
-        <option value="0.1" />
-        <option value="0.2" />
-        <option value="0.3" />
-        <option value="0.4" />
-        <option value="0.5" label="50%" />
-        <option value="0.6" />
-        <option value="0.7" />
-        <option value="0.8" />
-        <option value="0.9" />
-        <option value="1.0" label="100%" />
-      </datalist>
     </div>
   )
 }
